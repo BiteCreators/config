6f484a8942a664134bbcbd78b57cf074f28051ef
tapuru <pogladtapira@gmail.com>
2024-12-08 19:38:47 +0500

    init: init config repo

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    jest: true,
+  },
+  extends: [
+    "@it-incubator/eslint-config",
+    "plugin:storybook/recommended",
+    "plugin:testing-library/react",
+    "plugin:jest-dom/recommended",
+  ],
+  rules: {
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "react/button-has-type": "off",
+  },
+};
---
+++ index.cjs
@@ -0,0 +1,13 @@
+const eslintConfig = require("./.eslintrc.cjs");
+const stylelintConfig = require("./.stylelintrc.cjs");
+const prettierConfig = require("./.prettierrc.cjs");
+const tsConfig = require("./tsconfig.base.json");
+const tailwindConfig = require("./tailwind.config.cjs");
+
+module.exports = {
+  eslintConfig,
+  stylelintConfig,
+  prettierConfig,
+  tsConfig,
+  tailwindConfig
+};
