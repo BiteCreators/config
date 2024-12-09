480c89ae6702c5993c4d17ef3acd8bf705c93f44
tapuru <pogladtapira@gmail.com>
2024-12-09 17:09:52 +0500

    fix: fix index.js

--- index.cjs
+++ index.cjs
@@ -1,9 +1,10 @@
 const path = require('path');
+const tailwind = require('./tailwind.config.base.cjs')
 
 module.exports = {
   eslint: path.join(__dirname, '.eslintrc.cjs'),
   prettier: path.join(__dirname, '.prettierrc.cjs'),
-  tailwind: path.join(__dirname, 'tailwind.config.base.cjs'),
+  tailwind,
   stylelint: path.join(__dirname, '.stylelintrc.cjs'),
   tsconfig: path.join(__dirname, 'tsconfig.base.json'),
 };
